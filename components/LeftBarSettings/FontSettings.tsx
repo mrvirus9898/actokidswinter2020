ad3b28df50a5fa7d68a04aaa5f4b8e781437f884
Nick Bennett <jrtngb@yahoo.com>
2021-01-28 00:07:21 -0800

    Switched Spinners for less Jank

--- components/LeftBarSettings/FontSettings.tsx
+++ components/LeftBarSettings/FontSettings.tsx
@@ -1,40 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text,TouchableOpacity,
-    TouchableHighlight, View, Image } from 'react-native';
+import { View, Text } from 'react-native';
 import NumericInput from 'react-native-numeric-input'
+import InputSpinner from "react-native-input-spinner";
 
-import {
-    Input,
-    Card,
-    Button
-} from 'react-native-elements'
+import { Card } from 'react-native-elements'
 
 export default function FontSettings(){
 
-    let fontValue = 0;
+    let fontValue: number = 0;
 
-    function setValue(value){
-        fontValue = value;
+    function setValue(num){
+        fontValue = num;
         console.log(fontValue);
     }
 
     return(
         <Card>
             <View>
-            <NumericInput 
-                value={fontValue} 
-                onChange={value => setValue({value})} 
-                onLimitReached={(isMax,msg) => console.log(isMax,msg)}
-                totalWidth={240} 
-                totalHeight={50} 
-                iconSize={25}
-                step={4}
-                valueType='real'
-                rounded 
-                textColor='#B0228C' 
-                iconStyle={{ color: 'white' }} 
-                rightButtonBackgroundColor='#EA3788' 
-                leftButtonBackgroundColor='#E56B70'/>
+                <Text>
+                    <InputSpinner
+                        max={10}
+                        min={0}
+                        step={2}
+                        colorMax={"#f04048"}
+                        colorMin={"#40c5f4"}
+                        value={0}
+                        onChange={(num) => {
+                            console.log(num);
+                        }}
+                    />
+                </Text>
             </View>
         </Card>
 
