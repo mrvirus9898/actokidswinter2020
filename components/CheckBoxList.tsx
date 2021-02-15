faeb81bae5e94e7679aab4ae5a69d6dc9d0536a3
Nick Bennett <jrtngb@yahoo.com>
2021-02-15 14:28:08 -0800

    Implimented Checks

--- components/CheckBoxList.tsx
+++ components/CheckBoxList.tsx
@@ -10,7 +10,7 @@ TODO: Fix checkedIcon / uncheckedIcon not working
 
 */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View
 } from 'react-native';
@@ -19,15 +19,41 @@ import { CheckBox } from 'react-native-elements';
 
 export default function checkBoxList({title}){
 
+    const [filterCriteria, setFilter] = useState([]);
+    //Checked only true purpose is to force a state update on the checkbox component after the filter has been modified.
+    //It does not actually track what has or has not been checked. That is done by logic.
+    const [checked, setChecked] = useState(false);
+
+    //console.log(title)
+
     return(
         <View>
             {
                 title.map((item, key) => <CheckBox
                     title={item.value}
                     key={key}
+                    checked={filterCriteria.includes(item.value)}
                     checkedIcon='dot-circle-o'
-                    uncheckedIcon='circle-o' />)
+                    uncheckedIcon='circle-o' 
+                    onPress={() => changeCheck(item)}/>)
             }
         </View>        
     )
+
+    function changeCheck(item){
+        changeFilter(item.value)
+        setChecked(!checked)
+        console.log(filterCriteria)
+    }
+
+    function changeFilter(item){
+        //console.log(filterCriteria.indexOf(item))
+        //console.log(filterCriteria.includes(item))
+        if(!filterCriteria.includes(item)){
+            filterCriteria.push(item)
+        }else{
+            filterCriteria.pop(item)
+        }
+        
+    }
 }
