2e6930dbce25aa9b604277b8a110cd223a0ecb5c
Nick Bennett <jrtngb@yahoo.com>
2021-03-15 22:10:12 -0700

    SQLite functions continue

--- hooks/databaseFunctions.tsx
+++ hooks/databaseFunctions.tsx
@@ -8,14 +8,56 @@ export function createTable(tableName: string, name: [], datatype: []){
      /*DB.transaction(tx => {
       tx.executeSql("CREATE TABLE taxonomy (id number, value varchar(20));", [], success)
     })*/
-    let createTableString = "CREATE TABLE IF NOT EXISTS " + tableName + " ("
-    for (let i = 0; i < name.length; i++){
-        if(i == 0){
-            createTableString = createTableString + name[i] + " " + datatype[i]
-        }else{
-            createTableString = createTableString + ", " + name[i] + " " + datatype[i]
+    if(name.length == datatype.length)
+    {
+        let createTableString = "CREATE TABLE IF NOT EXISTS " + tableName + " ("
+        for (let i = 0; i < name.length; i++){
+            if(i == 0){
+                createTableString += name[i] + " " + datatype[i]
+            }else{
+                createTableString += ", " + name[i] + " " + datatype[i]
+            }
+        }
+        createTableString += ")"
+        console.log(createTableString)
+    }else{
+        console.log("Name and Datatype array length does not match")
+    }
+    return(null)
+}
+
+export function insertTable(tableName: string, columns: [], values: []){
+    let insertValuesStrings = "INSERT INTO " + tableName + " ("
+    let questionMarks = "( "
+    let dataToInsert = "('"
+    let dataSquences = values.length / columns.length
+    let dataSplit = 0
+
+    for (let i = 0; i < columns.length; i++){
+        insertValuesStrings += columns[i]
+        questionMarks += "?"
+        if(i != (columns.length-1)){
+            insertValuesStrings += ", "
+            questionMarks += ", "
         }
     }
-    createTableString = createTableString + ")"
+    insertValuesStrings += ") VALUES "
+    insertValuesStrings += questionMarks + ")"
+    console.log(insertValuesStrings)
+
+    for(let i = 0; i < values.length; i++){
+        dataToInsert += columns[i] + "'"
+        dataSplit += 1
+        if(dataSplit != columns.length){
+            dataToInsert += ",'"
+        }else{
+            if(i != (values.length-1)){
+                dataToInsert += "),('"
+            }else{
+                dataToInsert += ")"
+            }
+        }
+    }
+    console.log(dataToInsert)
     return(null)
 }
